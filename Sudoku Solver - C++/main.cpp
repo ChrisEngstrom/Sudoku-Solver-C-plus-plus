1fe45d973085e0f5544b11166790c5117dfad990
Chris Engstrom <tipher88@gmail.com>
2011-08-06 18:04:40 -0500

    Working on getting file input and displaying it from the gameBoard array like a sudoku puzzle.

--- Sudoku Solver - C++/main.cpp
+++ Sudoku Solver - C++/main.cpp
@@ -11,14 +11,16 @@ using namespace std;
 
 int main () {
 
-	char buffer[81];
+	char buffer[18],
+		 gameBoard[9][9];
+	int rowCount = 0;
 
-	ifstream inFile("puzzleInput.txt");
+	ifstream inFile("sudokuInput.txt");
 	ofstream outFile;
-	outFile.open("puzzleOutput.txt");
+	outFile.open("sudokuOutput.txt", ios::out | ios::trunc);
 
 	// Read first line of the inFile
-	inFile.getline(buffer, 80, '\n');
+	inFile.getline(buffer, 17, '\n');
 
 	if(!inFile)
 		cout << "Cannot locate file." << endl;
@@ -26,14 +28,65 @@ int main () {
 	{
 		while(!inFile.eof())
 		{
+			int bufferIndex = 0,
+				columnIndex = 0;
+
+			cout << buffer << endl;
+
+			while(buffer[bufferIndex] != '\n')
+			{
+				//***************
+				//* skip spaces *
+				//***************
+				while(buffer[bufferIndex] == ' ' && buffer[bufferIndex] != '\n')
+				{
+					bufferIndex++;
+				}
+
+				if(buffer[bufferIndex] != '\n')
+				{
+					gameBoard[rowCount][columnIndex] = buffer[bufferIndex];
+					columnIndex++;
+					bufferIndex++;
+				}
+			}
+
 			outFile << buffer << endl;
 
-			inFile.getline(buffer, 80, '\n');
+			inFile.getline(buffer, 17, '\n');
+			rowCount++;
 		}
 
-	}
+		cout << endl << endl;
 
-	cout << "This will fun!" << endl;
+		// Print out the contents of the gameBoard array in the form of a sudoku puzzle
+		for(int row = 0; row < 9; row++)
+		{
+			for(int column = 0; column < 9; column++)
+			{
+				cout << gameBoard[row][column];
+
+				if(column != 8)
+				{
+					cout << ' ';
+				}
+
+				if(!((column + 1) % 3) && column != 8)
+				{
+					cout << "| ";
+				}
+			}
+
+			if(!((row + 1) % 3) && row != 8)
+			{
+				cout << endl << "---------------------";
+			}
+
+			cout << endl;
+		}
+		
+
+	}
 
 	return 0;
 }
